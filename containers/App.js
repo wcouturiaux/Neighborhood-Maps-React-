4d7df5b95544caabfde94b21b580e31fb7c3f18a
Will C <w.l.couturiaux@gmail.com>
2019-02-05 23:01:47 -0500

    Change default export

--- containers/App.js
+++ containers/App.js
@@ -1,8 +1,14 @@
 import React, {Component} from 'react';
 import  {Map, GoogleApiWrapper} from 'google-maps-react';
 
-export default class App extends Component {
+export class App extends Component {
     render () {
-        return <Map google={this.props.google}/>
+        return (
+          <Map google={this.props.google}/>
+        );
     }
 }
+
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyAi7ZcWjqIbKWWF1PSCKESKsN4W81AzASM'
+})(App)
