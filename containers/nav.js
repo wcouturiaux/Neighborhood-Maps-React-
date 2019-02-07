f056b86525a6480ad76bad6d52a6ab2045da6258
Will C <w.l.couturiaux@gmail.com>
2019-02-06 23:29:18 -0500

    Add Nav component

---
+++ containers/nav.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import style from '../styles/menu.less';
+
+export default class Nav extends Component {
+
+  render () {
+    return(
+      <div className={style.nav}>
+        Hello!
+      </div>
+    )
+  }
+}
