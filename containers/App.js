37ac1a09c1aec90b63ed265452fde2823570e398
Will C <w.l.couturiaux@gmail.com>
2019-02-07 22:23:12 -0500

    Add additional imports and state

--- containers/App.js
+++ containers/App.js
@@ -1,15 +1,28 @@
 import React, {Component} from 'react';
-import  {Map, GoogleApiWrapper} from 'google-maps-react';
+import  {Map, GoogleApiWrapper, InfoWindow, Marker} from 'google-maps-react';
 import Nav from './nav';
 import mapStyle from '../styles/map.less'
 
 export class App extends Component {
+
+  state = {
+    showingInfoWindow: false, //Manage info window visibility
+    activeMarker: {},         //Manage active marker
+    selectedPlace: {}        //Show info window on marker
+  }
     render () {
         return (
           <div className={mapStyle.container}>
             <Nav />
             <div className={mapStyle.map}>
-              <Map google={this.props.google}/>
+              <Map
+                google={this.props.google}
+                zoom={12}
+                initialCenter={{
+                  lat: 42.3314,
+                  lng: -83.0458
+                }}
+              />
             </div>
           </div>
         );
